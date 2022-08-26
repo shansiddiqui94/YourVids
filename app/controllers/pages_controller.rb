6b12c3a1650aac309759d0729870804bfbe7989f
Shan Siddiqui <49912126+shansiddiqui94@users.noreply.github.com>
2022-08-26 12:16:00 -0400

    updated home page, fixed devise

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,7 +9,6 @@ class PagesController < ApplicationController
 
   def home
     @courses = Course.where(public: true) 
-    # Orginally I had all courses that made it through to Public files, now where public checkbox is true we can only select those videos. 
   end
 
   def landing
